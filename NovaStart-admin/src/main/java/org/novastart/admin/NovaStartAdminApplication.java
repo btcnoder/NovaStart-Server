af2656e031f290ce38f190ddff6c2c3792193665
btcnoder <btcnoder@gmail.com>
2022-09-18 21:10:02 +0000

    add aop support

--- NovaStart-admin/src/main/java/org/novastart/admin/NovaStartAdminApplication.java
+++ NovaStart-admin/src/main/java/org/novastart/admin/NovaStartAdminApplication.java
@@ -2,10 +2,11 @@ package org.novastart.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.novastart.*"})
 public class NovaStartAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NovaStartAdminApplication.class, args);
     }
---
+++ NovaStart-admin/src/main/java/org/novastart/admin/model/User.java
@@ -0,0 +1,12 @@
+package org.novastart.admin.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    // 用户名
+    private String username;
+    // 性别
+    private Integer sex;
+}
+
